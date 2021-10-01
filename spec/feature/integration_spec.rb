cfa3e2949bc2e7d2d57f0dffe1fa59868e1f46f0
root <root@localhost>
2021-10-01 02:23:45 +0000

    Merge conflicts somehow

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe 'Creating a book', type: :feature do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
     fill_in 'Author', with: 'jk'
-    fill_in 'Date', with: '01/01/2021'
     fill_in 'Price', with: '3.50'
+    fill_in 'Date', with: '01/01/2021'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')

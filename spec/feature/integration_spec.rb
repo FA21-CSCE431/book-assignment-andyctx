e015e1f87c3901611fa7ab2731c8a74433ee7587
root <root@localhost>
2021-10-01 02:19:00 +0000

    Split integration test

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -15,4 +15,37 @@ RSpec.describe 'Creating a book', type: :feature do
     expect(page).to have_content('2021-01-01')
     expect(page).to have_content('3.50')
   end
+
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'jk'
+    fill_in 'Date', with: '01/01/2021'
+    fill_in 'Price', with: '3.50'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('jk')
+  end
+
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'jk'
+    fill_in 'Date', with: '01/01/2021'
+    fill_in 'Price', with: '3.50'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('2021-01-01')
+  end
+
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'jk'
+    fill_in 'Date', with: '01/01/2021'
+    fill_in 'Price', with: '3.50'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('3.50')
+  end
 end
